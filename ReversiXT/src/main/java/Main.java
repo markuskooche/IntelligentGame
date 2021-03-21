6f9fca58cec9763abdb226e4d579e688bf3fa070
Markus Koch <markuskoch.social@icloud.com>
2021-03-21 21:34:11 +0100

    added basic game classes

--- ReversiXT/src/main/java/Main.java
+++ ReversiXT/src/main/java/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Game game = new Game("ReversiXT/src/main/resources/bomb.map");
-        game.board.inversion(2);
         System.out.println(game.toString());
     }
 }
