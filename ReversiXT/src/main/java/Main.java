bd268dc0327394944da72c47574b22070926dd71
Markus Koch <markuskoch.social@icloud.com>
2021-03-23 11:47:51 +0100

    deleted files

--- ReversiXT/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import client.Game;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Game game = new Game("ReversiXT/src/main/resources/europa2.map");
-        System.out.println(game.toString());
-    }
-}
