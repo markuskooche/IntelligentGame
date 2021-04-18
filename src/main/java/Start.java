a700d66338565295e8cb443f75b635a1ac7f507a
Markus Koch <markuskoch.social@icloud.com>
2021-04-18 14:24:31 +0200

    added a Start class to change a game field manually

---
+++ src/main/java/Start.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Start {
+
+    private static Game createGame(String filename){
+        Game game = null;
+        Path path = Paths.get(filename);
+
+        try {
+            List<String> file = Files.lines(path).collect(Collectors.toList());
+            game = new Game(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return game;
+    }
+
+    public static void main(String[] args) {
+        Game game = createGame("maps/initialMaps/bomb.map");
+        System.out.println(game.toString());
+
+        Player player = game.getPlayer(1);
+
+        System.out.println(player);
+        game.getBoard().executeMove(player, true);
+        System.out.println(player);
+    }
+}
