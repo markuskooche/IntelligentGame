6bd7d04af6bd5e2fcca88da4a41caf05f0661ed2
Iwan Eckert <iwan.eckert@st.oth-regensburg.de>
2021-06-15 17:00:11 +0200

    added sort wallLines and goodLines

--- src/main/java/heuristic/Line.java
+++ src/main/java/heuristic/Line.java
@@ -8,10 +8,19 @@ public class Line {
 
     private Move move;
     private List<Character> stoneRow;
+    private int moveValue;
 
     public Line(Move move, List<Character> stones) {
         this.move = move;
         this.stoneRow = stones;
+    }
+
+    public int getMoveValue() {
+        return moveValue;
+    }
+
+    public void setMoveValue(int moveValue) {
+        this.moveValue = moveValue;
     }
 
     public Move getMove() {
