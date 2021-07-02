a7985e45c23a96e5d0de2fa7387f192159660019
Benedikt Halbritter <benedikt.halbritter@st.oth.regensburg.de>
2021-07-02 16:16:23 +0200

    fixed Mapvalues and added Factor support

--- src/main/java/heuristic/Heuristics.java
+++ src/main/java/heuristic/Heuristics.java
@@ -585,8 +585,8 @@ public class Heuristics {
             mapAnalyzer.createField();
             createdReachableFields = true;
             //TODO im Auge behalten
-            System.out.println("Geschafft_______________________________________________________________");
-            System.out.println(mapAnalyzer.getBoardValues());
+           // System.out.println("Geschafft_______________________________________________________________");
+           // System.out.println(mapAnalyzer.getBoardValues());
         }
 
     }
